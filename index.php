2e69c3d788b72267648b850c009f1be1383e4528
Mchumbles <lewis.mcguire92@yahoo.com>
2025-02-03 13:41:38 +0000

    Initial commit of basic structure

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body>
+    <h1>Welcome to user authentication project</h1>
+        <p><a href="public/logout.php">Logout</a></p>
+        <p>You are not logged in.</p>
+        <p><a href="public/login.html">Login</a> | <a href="public/register.html">Register</a></p>
+</body>
+</html>
